7d444bc61ecef3005401055b8391694beed60e49
nuwan <nuwansiri.dev@gmail.com>
2026-04-13 23:22:04 +0530

    Create simple endpoint with test case

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class UserController extends Controller
+{
+    public function getAllUsers(){
+        return response()->jason([
+            'users' =>[
+            [
+                'id' => 1,
+                'name' => 'jhone Deo',
+                'age' => '19',
+                'role' => 'Admin'
+            ],
+            [
+                'id' => 2,
+                'name' => 'janith Deo',
+                'age' => '20',
+                'role' => 'Staff'
+            ],
+            [
+                'id' => 3,
+                'name' => 'gorhta',
+                'age' => '39',
+                'role' => 'User'
+            ]
+
+
+            ]
+        ]);
+    }
+}
